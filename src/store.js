45cb4f87a21435075328e32ce80ac2cb8601b23d
pb <734972743@qq.com>
2019-08-28 03:09:50 +0800

    初始化

--- src/store.js
+++ src/store.js
@@ -4,10 +4,16 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  // state 是用来管理vuex里面的所有的状态值的
   state: {
-
+    count: 0
   },
+
+  // 用来管理state值的方法
   mutations: {
+    increase () {
+      this.state.count++
+    }
 
   },
   actions: {
---
+++ src/views/Info.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+  <div>info</div>
+ <button type="button" @click="increase()">点我</button>
+</div>
+
+</template>
+<script>
+import store from "@/store.js"
+
+export default {
+  name:"Info",
+
+  methods:{
+    increase(){
+      debugger
+      store.commit("increase");
+    },
+
+    output(){
+      alert("output")
+    }
+  },
+
+
+  mounted (){
+    window.vue = this  // 把this 绑定给window.vue 这样可方便调试， 以后调试时，
+    //在console窗口是直接用 window.vue 可直接调试
+  }
+}
+</script>
+
+<style  scoped>
+
+</style>
